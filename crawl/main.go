135c0edbb07830da6bad1b50ebf9b0eab08a12de
Choi Yeongjun <frozenmoai@gmail.com>
2020-02-14 01:09:22 +0900

    Add crawl package skeleton

---
+++ crawl/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking url :", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
+}
